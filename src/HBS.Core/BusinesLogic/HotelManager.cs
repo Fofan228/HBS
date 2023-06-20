8e2eb6c9b82c6396f848ae543d634529b3df9574
Savelus <savelussava@mail.ru>
2023-06-20 12:40:59 +0500

    change float to double

--- src/HBS.Core/BusinesLogic/HotelManager.cs
+++ src/HBS.Core/BusinesLogic/HotelManager.cs
@@ -4,28 +4,28 @@ namespace HBS.Core.BusinesLogic
 {
     public class HotelManager
     {
-        public List<HotelModel> GetNearbyHotels(float longitude, float latitude, float radius){
+        public List<HotelModel> GetNearbyHotels(double longitude, double latitude, double radius){
             return HotelsModel.Hotels
                         .Where(h => DistanceBetweenObjects(longitude, latitude, h.Longitude, h.Latitude) < radius)
                         .OrderBy(h => h.Rating)
                         .ToList();
         }
 
-        public HotelModel GetBestHotel(float longitude, float latitude, float radius){
+        public HotelModel GetBestHotel(double longitude, double latitude, double radius){
             return HotelsModel.Hotels
                         .Where(h => DistanceBetweenObjects(longitude, latitude, h.Longitude, h.Latitude) < radius)
                         .OrderBy(h => h.Rating)
                         .First();
         }
 
-        public HotelModel GetNearestHotel(float longitude, float latitude){
+        public HotelModel GetNearestHotel(double longitude, double latitude){
             return HotelsModel.Hotels
                         .OrderBy(h => DistanceBetweenObjects(longitude, latitude, h.Longitude, h.Latitude))
                         .First();
         }
 
-        private float DistanceBetweenObjects(float startX, float startY, float endX, float endY){
-            return MathF.Sqrt(MathF.Pow(startX - endX, 2) + MathF.Pow(startX - endX, 2));
+        private double DistanceBetweenObjects(double startX, double startY, double endX, double endY){
+            return Math.Sqrt(Math.Pow(startX - endX, 2) + Math.Pow(startX - endX, 2));
         }
     }
 }
--- src/HBS.Core/BusinesLogic/NearbyPlaceManager.cs
+++ src/HBS.Core/BusinesLogic/NearbyPlaceManager.cs
@@ -4,35 +4,35 @@ namespace HBS.Core.BusinesLogic
 {
     public class NearbyPlaceManager
     {
-        public List<NearbyPlaceModel> GetNearbyPlaces(HotelModel hotel, float radius){
+        public List<NearbyPlaceModel> GetNearbyPlaces(HotelModel hotel, double radius){
             return NearbyPlacesModel.NearbyPlaces
                         .Where(p => DistanceBetweenObjects(hotel.Longitude, hotel.Latitude, p.Longitude, p.Latitude) < radius)
                         .OrderBy(p => DistanceBetweenObjects(hotel.Longitude, hotel.Latitude, p.Longitude, p.Latitude))
                         .ToList();
         }
 
-        public NearbyPlaceModel GetNearestPlace(HotelModel hotel, float radius){
+        public NearbyPlaceModel GetNearestPlace(HotelModel hotel, double radius){
             return NearbyPlacesModel.NearbyPlaces
                         .Where(p => DistanceBetweenObjects(hotel.Longitude, hotel.Latitude, p.Longitude, p.Latitude) < radius)
                         .OrderBy(p => DistanceBetweenObjects(hotel.Longitude, hotel.Latitude, p.Longitude, p.Latitude))
                         .First();
         }
 
-        public List<NearbyPlaceModel> GetNearbyPlaces(float longitude, float latitude, float radius){
+        public List<NearbyPlaceModel> GetNearbyPlaces(double longitude, double latitude, double radius){
             return NearbyPlacesModel.NearbyPlaces
                         .Where(p => DistanceBetweenObjects(longitude, latitude, p.Longitude, p.Latitude) < radius)
                         .OrderBy(p => DistanceBetweenObjects(longitude, latitude, p.Longitude, p.Latitude))
                         .ToList();
         }
 
-        public NearbyPlaceModel GetNearestPlace(float longitude, float latitude, float radius){
+        public NearbyPlaceModel GetNearestPlace(double longitude, double latitude, double radius){
             return NearbyPlacesModel.NearbyPlaces
                         .Where(p => DistanceBetweenObjects(longitude, latitude, p.Longitude, p.Latitude) < radius)
                         .OrderBy(p => DistanceBetweenObjects(longitude, latitude, p.Longitude, p.Latitude))
                         .First();
         }
-        private float DistanceBetweenObjects(float startX, float startY, float endX, float endY){
-            return MathF.Sqrt(MathF.Pow(startX - endX, 2) + MathF.Pow(startX - endX, 2));
+        private double DistanceBetweenObjects(double startX, double startY, double endX, double endY){
+            return Math.Sqrt(Math.Pow(startX - endX, 2) + Math.Pow(startX - endX, 2));
         }
     }
 }
